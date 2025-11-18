4c43aa4924205b289b7bd9f18c47f99507820355
Eugene <deadrime@yandex.ru>
2025-11-18 16:45:18 +0300

    Revert "Merge branch 'main' of github.com:storm-trade/sdk-js"
    This reverts commit 0d38c5b25bf6af20e9ca3b3f3ee24dffd52d7222, reversing
    changes made to 9a6704a0329bdff7759a8f49459d901bddb9086a.

--- src/base-packers/oracle-packer.ts
+++ src/base-packers/oracle-packer.ts
@@ -1,34 +1,21 @@
 import { beginCell, Cell } from '@ton/ton';
 import { OraclePayload } from './oracle-packer.types';
-import { packLazerMessage } from './lazer-packers';
 
 export function packOraclePayload(data: OraclePayload): Cell {
   switch (data.oraclePayloadKind) {
     case 'simple':
       return beginCell()
         .storeUint(0, 8)
-        .storeRef(beginCell()
-          .storeRef(data.priceRef)
-          .storeRef(data.signaturesRef)
-          .endCell()
-        )
-        .storeMaybeRef(data.lazerMessage ? packLazerMessage(data.lazerMessage) : null)
+        .storeRef(data.priceRef)
+        .storeRef(data.signaturesRef)
         .endCell();
     case 'withSettlement':
       return beginCell()
         .storeUint(1, 8)
-        .storeRef(
-          beginCell()
-            .storeRef(data.priceRef)
-            .storeRef(data.signaturesRef)
-            .storeRef(data.settlementPriceRef)
-            .storeRef(data.settlementSignaturesRef)
-            .endCell()
-        )
-        .storeMaybeRef(data.lazerMessage ? packLazerMessage(data.lazerMessage) : null)
+        .storeRef(data.priceRef)
+        .storeRef(data.signaturesRef)
+        .storeRef(data.settlementPriceRef)
+        .storeRef(data.settlementSignaturesRef)
         .endCell();
-    default:
-      // @ts-expect-error expect ts error
-      throw new Error('Unknown oracle payload kind: ' + data.oraclePayloadKind);
   }
 }
--- src/base-packers/oracle-packer.types.ts
+++ src/base-packers/oracle-packer.types.ts
@@ -1,21 +1,16 @@
 import { Cell } from '@ton/ton';
-import { Maybe } from '@ton/core/dist/utils/maybe';
-import { LazerMessage } from './lazer-packers.types';
 
 export type SimpleOraclePayload = {
   oraclePayloadKind: 'simple';
   priceRef: Cell;
   signaturesRef: Cell;
-  lazerMessage?: Maybe<LazerMessage>;
 };
-
 export type SettlementOraclePayload = {
   oraclePayloadKind: 'withSettlement';
   priceRef: Cell;
   signaturesRef: Cell;
   settlementPriceRef: Cell;
   settlementSignaturesRef: Cell;
-  lazerMessage?: Maybe<LazerMessage>;
 };
 
 export type OraclePayload =
